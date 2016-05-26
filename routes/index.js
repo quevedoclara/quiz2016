eb482604925ec2f9ba6a6dbadef0f400a683261b
quevedoclara <clara.quevedo.galan@alumnos.upm.es>
2016-05-26 02:18:58 +0200

    página de autores en la rama créditos

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res, next) {
+	res.render('author', { title: 'Author'});
+});
+
 module.exports = router;
